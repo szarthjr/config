3e72d09952c8514eac5a5e3df307c4cfb1adfbb5
Sergio Zarth Jr <sergio.zarth@softplan.com.br>
2019-06-24 09:37:33 -0300

    commit inicial

---
+++ config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	csvImageFilePath string
+	hasHead bool
+
+}
+
+// New returns a new Config struct
+func New() *Config {
+	return &Config{
+		hasHead: getEnvAsBool("HASHEADER", true),
+		csvImageFilePath: getEnv("CSVIMAGEFILE", "./pair_dataset_image.csv"),
+	}
+}
+
+// Simple helper function to read an environment or return a default value
+func getEnv(key string, defaultVal string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+
+	return defaultVal
+}
+
+func getEnvAsBool(name string, defaultVal bool) bool {
+	valStr := getEnv(name, "")
+	if val, err := strconv.ParseBool(valStr); err == nil {
+		return val
+	}
+
+	return defaultVal
+}
+
+// Helper to read an environment variable into a string slice or return default value
+func getEnvAsSlice(name string, defaultVal []string, sep string) []string {
+	valStr := getEnv(name, "")
+
+	if valStr == "" {
+		return defaultVal
+	}
+
+	val := strings.Split(valStr, sep)
+
+	return val
+}
+
+// Simple helper function to read an environment variable into integer or return a default value
+func getEnvAsInt(name string, defaultVal int) int {
+	valueStr := getEnv(name, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+
+	return defaultVal
+}
